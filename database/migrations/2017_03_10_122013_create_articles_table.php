bd55e6db4a7cfd08dc25a4c51e4d5ac1b89b76b2
student <student@Win64-4.ue.loc>
2017-03-10 13:57:40 +0100

    add migrations

---
+++ database/migrations/2017_03_10_122013_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->referemces('id')->on('categories');
+            $table->string('title');
+            $table->text('contect');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
